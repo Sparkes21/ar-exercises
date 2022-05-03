5c8e5f8b21d7a19b5598d689929a0c3036ecc8b1
Josh Sparkes <jshsparx@gmail.com>
2022-05-02 18:36:19 -0600

    complete exercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,7 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, presence: true
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: {only_integer: true}
+  validates :annual_revenue, numericality: {greater_than_equal_to: 0}
 end
